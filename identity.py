98990a7c9766e3633d6f485b1f5f566235e635db
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-07-22 18:14:10 +0100

    Update identity.py

--- identity.py
+++ identity.py
@@ -1,7 +1,6 @@
 
 class IdentityContext:
     def __init__(self, atomix, identity_id):
-        self.atomix = atomix
         self.identity_id = identity_id
 
     def __enter__(self):
@@ -10,23 +9,23 @@ class IdentityContext:
     def __exit__(self, exc_type, exc_value, traceback):
         pass
 
-    def commit(self):
-        self.atomix.commit(self.identity_id)
+    def commit(self, atomix):
+        atomix.commit(self.identity_id)
 
-    def abort(self):
-        self.atomix.abort(self.identity_id)
+    def abort(self, atomix):
+        atomix.abort(self.identity_id)
 
-    def read(self, key):
-        return self.atomix.read(self.identity_id, key)
+    def read(self, key, atomix):
+        return atomix.read(self.identity_id, key)
 
-    def write(self, key, value):
-        self.atomix.write(self.identity_id, key, value)
+    def write(self, key, value, atomix):
+        atomix.write(self.identity_id, key, value)
 
-    def delete(self, key):
-        self.atomix.delete(self.identity_id, key)
+    def delete(self, key, atomix):
+        atomix.delete(self.identity_id, key)
 
-    def store(self, key, value):
-        self.atomix.store(self.identity_id, key, value)
+    def store(self, key, value, atomix):
+        atomix.store(self.identity_id, key, value)
 
-    def offload_store(self):
-        self.atomix.offload_store(self.identity_id)
+    def offload_store(self, atomix):
+        atomix.offload_store(self.identity_id)
