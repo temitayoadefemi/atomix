3bc6efacd1a2975f57e0ea840696e3b5ccc1c53c
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-07-23 10:28:02 +0100

    Update version.py

--- version.py
+++ version.py
@@ -1,5 +1,6 @@
-class VersionedValue():
-    def __init__(self, value, version):
+class VersionedData:
+    def __init__(self, value, version, committed=False):
         self.value = value
         self.version = version
+        self.committed = committed
         self.deleted = False
